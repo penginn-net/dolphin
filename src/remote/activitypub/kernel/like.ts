64346ca3ed8d5a4ee8dec729bc51fcbf59699dc6
Pen <pen@penginn.net>
2026-06-18 23:17:52 +0900

    like処理時、常にResolveを試みるように

--- src/remote/activitypub/kernel/like.ts
+++ src/remote/activitypub/kernel/like.ts
@@ -1,12 +1,18 @@
 import { IRemoteUser } from '../../../models/entities/user';
 import { ILike, getApId } from '../type';
 import create from '../../../services/note/reaction/create';
-import { fetchNote, extractEmojis } from '../models/note';
+import { resolveNote, extractEmojis} from '../models/note';
+import { fetchMeta } from "../../../misc/fetch-meta";
+import { extractDbHost } from "../../../misc/convert-host";
 
 export default async (actor: IRemoteUser, activity: ILike) => {
 	const targetUri = getApId(activity.object);
 
-	const note = await fetchNote(targetUri);
+	//ブロックしているホストならResolveしない
+	const meta = await fetchMeta();
+	if (meta.blockedHosts.includes(extractDbHost(targetUri))) return 'skip: blockedHost'
+
+	const note = await resolveNote(targetUri);
 	if (!note) return `skip: target note not found ${targetUri}`;
 
 	if (actor.id === note.userId) return `skip: cannot react to my note`;
